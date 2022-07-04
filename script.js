3f13bd245d02be84cb2e1580b676b6dd35d067ec
Jane <jane.anqi.cui@gmail.com>
2022-07-03 21:25:11 -0400

    add tooltip with absolute positioning

--- script.js
+++ script.js
@@ -15,31 +15,36 @@ async function handleData() {
     .domain(scoreExtents)
     .range([500, 0])
 
-  // // line generator
-  // const linegen = d3.line()
-  // .x(d => xscale(d.GDPPC))
-  // .y(d => yscale(d.score))
-  // .curve(d3.curveLinear)
-
   const svg = d3
   .select('#svg')
+  .append('svg')
+    .attr('width', 1090)
+    .attr('height', 540)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
 
-  //   // line generator
-  // const linegen = d3.line() // create a line generator
-  // .x((d) => xscale(parseFloat(d.GDPPC))) // map the x to the index
-  // .y(d => yscale(parseFloat(d.score))) // map y to percipitation
-  // .curve(d3.curveLinear) // apply a curve type
+  var mouseover = function(d) {
+    tooltip
+      .style("opacity", 1)
+  }
 
-  // graph
-  // .append('path') // append a path
-  // .attr('d', linegen(data)) // generate a line
-  // .attr('stroke-width', 3)
-  // .attr('stroke', 'cornflowerblue')
-  // .attr('fill', 'none')
+  var mousemove = function(e, data) {
+    tooltip
+      .html( `Country: ${data.region}<br>Happiness Score: ${data.score}<br>GDP per Capita: ${data.GDPPC}`)
+      .style("left", `${d3.pointer(e)[0]}px`) // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+      .style("top",  `${d3.pointer(e)[1]+150}px`)
+  }
+
+  // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+  var mouseleave = function(d) {
+    tooltip
+      .transition()
+      .duration(200)
+      .style("opacity", 0)
+  }
+
 
   const bottomAxis = d3.axisBottom(xscale)
   const leftAxis = d3.axisLeft(yscale)
@@ -52,11 +57,14 @@ async function handleData() {
   .data(data)
   .enter()
   .append('circle')
-  .attr('cx', d => xscale(parseFloat(d.GDPPC)))
-  .attr('cy', d => yscale(parseFloat(d.score)))
-  .attr('r', 8)
-  .style('fill', '#003800')
-  .style('opacity', 0.3)
+    .attr('cx', d => xscale(parseFloat(d.GDPPC)))
+    .attr('cy', d => yscale(parseFloat(d.score)))
+    .attr('r', 8)
+    .style('fill', '#003800')
+    .style('opacity', 0.3)
+  .on('mouseover', mouseover)
+  .on('mousemove', (e, data) => mousemove(e, data))
+  .on('mouseleave', mouseleave)
 
   svg
   .append('g')
@@ -69,6 +77,17 @@ svg
   .append('g')
   .attr('transform', `translate(0, 0)`)
   .call(leftAxis)
+
+  var tooltip = d3.select("#svg")
+  .append("div")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("position", "absolute")
+  .style("border-width", "1px")
+  .style("border-radius", "5px")
+  .style("padding", "10px")
 
 }
 
